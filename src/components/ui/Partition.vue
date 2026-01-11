65b5f9811a6c490fa31860c33dadada6d354e69b
rHyThM3294 <SMILE32943294@gmail.com>
2026-01-11 12:25:39 +0800

    修復行動裝置時可以縮小看到整個分割線

--- src/components/ui/Partition.vue
+++ src/components/ui/Partition.vue
@@ -4,4 +4,26 @@
 <script setup>
 </script>
 <style scoped>
+.partition{
+    position: relative;
+    width: 100%;
+    overflow: hidden; 
+    top: -12vh;
+}
+.partition img{
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%) scale(0.7);
+}
+@media(width>=768px){
+    .partition{
+        position: relative;
+        width: 100%;
+        height: 175px;
+        top: -15vh;
+    }
+    .partition img{
+        transform: scaleX(1.5);
+    }
+}
 </style>
