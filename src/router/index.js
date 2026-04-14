ddcfd8cd6ace338f34b82e5b5224e03f1b169295
rHyThM3294 <smile32943294@gmail.com>
2026-04-14 17:23:57 +0800

    將index.js優化，刪除重複資訊

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const HomeView = () => import('@/views/customer/HomeView.vue')
 const ProductsView = () => import('@/views/customer/ProductsView.vue')
 const ProductsDetailView = () => import('@/views/customer/ProductsDetailView.vue')
@@ -10,37 +11,43 @@ const KnowledgeDetailView = () => import('@/views/customer/KnowledgeDetailView.v
 const CooperationFarmerView = () => import('@/views/customer/CooperationFarmerView.vue')
 const FarmerDetailView = () => import('@/views/customer/FarmerDetailView.vue')
 const MemberView = () => import('@/views/customer/MemberView.vue')
+
 const AdminLoginView = () => import('@/views/admin/AdminLoginView.vue')
 const AdminDashboardView = () => import('@/views/admin/AdminDashboardView.vue')
 const AddCommodityDetail = () => import('@/components/adminBlock/AddCommodityDetail.vue')
 const CheckOrder = () => import('@/components/adminBlock/CheckOrder.vue')
 const Commodity = () => import('@/components/adminBlock/Commodity.vue')
 const Withdraw = () => import('@/components/adminBlock/Withdraw.vue')
-const Finance = () =>import('@/components/adminBlock/Finance.vue')
+const Finance = () => import('@/components/adminBlock/Finance.vue')
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView, meta: { showLoading: true } },
     { path: '/products', name: 'products', component: ProductsView },
-    { path: '/productDetail/:category/:id', name: 'productDetail', component: () => import('../views/customer/ProductsDetailView.vue') },
-    { path: '/cart', component: CartView },
-    { path: '/map', component: MapSeasonView },
-    { path: '/season', component: MapSeasonView },
-    { path: '/ranking', component: RankView },
-    { path: '/knowledge', component: KnowledgeView },
+    { path: '/productDetail/:category/:id', name: 'productDetail', component: ProductsDetailView },
+
+    { path: '/cart', name: 'cart', component: CartView },
+    { path: '/member', name: 'member', component: MemberView },
+
+    { path: '/map', name: 'map', component: MapSeasonView },
+    { path: '/season', name: 'season', component: MapSeasonView },
+    { path: '/ranking', name: 'ranking', component: RankView },
+
+    { path: '/knowledge', name: 'knowledge', component: KnowledgeView },
     { path: '/knowledge/:id', name: 'knowledge-detail', component: KnowledgeDetailView },
+
     { path: '/cooperation-farmer', name: 'cooperation-farmer', component: CooperationFarmerView },
-    { path: '/farmer/:id', name: 'farmerDetail', component: () => import('../views/customer/FarmerDetailView.vue')},
-    { path: '/cart', name:'cart',component: () => import('@/views/customer/CartView.vue') },
-    { path: '/member', name:'member',component: () => import("@/views/customer/MemberView.vue") },
-    { path: '/admin/login', component: AdminLoginView },
+    { path: '/farmer/:id', name: 'farmerDetail', component: FarmerDetailView },
+
     { path: '/admin/login', name: 'adminLogin', component: AdminLoginView },
-    { path: '/admin/dashboard', component: AdminDashboardView },
-    { path: '/admin/add-commodity', component: AddCommodityDetail },
-    { path: '/admin/check-order', component: CheckOrder },
-    { path: '/admin/commodity', component: Commodity },
-    { path: '/admin/withdraw', component: Withdraw },
-    { path: '/admin/Finance', component: Finance },
+    { path: '/admin/dashboard', name: 'adminDashboard', component: AdminDashboardView },
+    { path: '/admin/add-commodity', name: 'addCommodity', component: AddCommodityDetail },
+    { path: '/admin/check-order', name: 'checkOrder', component: CheckOrder },
+    { path: '/admin/commodity', name: 'commodity', component: Commodity },
+    { path: '/admin/withdraw', name: 'withdraw', component: Withdraw },
+    { path: '/admin/finance', name: 'finance', component: Finance },
+
     { path: '/:pathMatch(.*)*', redirect: '/' },
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -49,8 +56,9 @@ const router = createRouter({
     }
     return {
       top: 0,
-      behavior: 'smooth'
+      behavior: 'smooth',
     }
-  }
+  },
 })
+
 export default router
